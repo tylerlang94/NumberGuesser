f3175a47fa850294cc4c5ff7810d3168ae7152a5
Tyler Lang <tylerlang@Tylers-Laptop.local>
2024-03-24 22:07:35 -0600

    formatted

--- Headers/Random.h
+++ Headers/Random.h
@@ -3,38 +3,41 @@
 
 #include <chrono>
 #include <random>
+//
+// Generates random number given the range
+//
+namespace Random {
+inline std::mt19937 generate() {
+  std::random_device rd{};
 
-namespace Random
-{
-    inline std::mt19937 generate()
-    {
-        std::random_device rd{};
+  std::seed_seq ss{
+      static_cast<std::seed_seq::result_type>(
+          std::chrono::steady_clock::now().time_since_epoch().count()),
+      rd(),
+      rd(),
+      rd(),
+      rd(),
+      rd(),
+      rd(),
+      rd()};
 
-        std::seed_seq ss{
-                static_cast<std::seed_seq::result_type>(std::chrono::steady_clock::now().time_since_epoch().count()),
-                rd(), rd(), rd(), rd(), rd(), rd(), rd() };
-
-        return std::mt19937{ ss };
-    }
-
-    inline std::mt19937 mt{ generate() }; // generates a seeded std::mt19937 and copies it into our global object
-
-    inline int get(int min, int max)
-    {
-        return std::uniform_int_distribution<>{min, max}(mt);
-    }
-
-    template <typename T>
-    T get(T min, T max)
-    {
-        return std::uniform_int_distribution<T>{min, max}(mt);
-    }
-
-    template <typename R, typename S, typename T>
-    R get(S min, T max)
-    {
-        return get<R>(static_cast<R>(min), static_cast<R>(max));
-    }
+  return std::mt19937{ss};
 }
+
+inline std::mt19937 mt{generate()}; // generates a seeded std::mt19937 and
+                                    // copies it into our global object
+
+inline int get(int min, int max) {
+  return std::uniform_int_distribution<>{min, max}(mt);
+}
+
+template <typename T> T get(T min, T max) {
+  return std::uniform_int_distribution<T>{min, max}(mt);
+}
+
+template <typename R, typename S, typename T> R get(S min, T max) {
+  return get<R>(static_cast<R>(min), static_cast<R>(max));
+}
+} // namespace Random
 
 #endif
---
+++ src/NumberGuesser.cpp
@@ -0,0 +1,67 @@
+#include "../Headers/Random.h"
+#include <iosfwd>
+#include <iostream>
+
+// TODO: Add error handling for the number inputs
+bool PlayGame(int guesses, int number) {
+  for (int count{1}; count <= guesses; ++count) {
+    std::cout << "Guess Number: " << count << '\n';
+    std::cout << "What is your guess: ";
+
+    int guess{};
+    std::cin >> guess;
+
+    if (guess > number) {
+      std::cout << "Your guess of " << guess << " is too high\n";
+    } else if (guess < number) {
+      std::cout << "Your guess of " << guess << " is too low\n";
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
+
+// TODO: Add input validation for y/n
+bool PlayAgain() {
+  while (true) {
+    char ch{};
+    std::cout << "Would you like to play again? (y/n): ";
+    std::cin >> ch;
+
+    switch (ch) {
+    case 'y':
+    case 'Y':
+      return true;
+    case 'n':
+    case 'N':
+      return false;
+    default:
+      PlayAgain();
+    }
+  }
+}
+
+int main() {
+  constexpr int guesses{7};
+
+  do {
+    int number{Random::get(1, 100)};
+
+    std::cout << "Lets play a game. I am thinking of a number between 1 and "
+                 "100. You have "
+              << guesses << " tries\n";
+
+    bool won{PlayGame(guesses, number)};
+
+    if (won) {
+      std::cout << "Correct! You Win! ";
+    } else {
+      std::cout << "Sorry but you lose! The correct number was " << number
+                << '\n';
+    }
+  } while (PlayAgain());
+
+  std::cout << "Thank you for playing! Come agian soon!";
+  return 0;
+}
